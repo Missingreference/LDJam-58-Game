ac66bf5d309dbd234d9bacb13d4d73572f801473
Missingreference <Missingreference@users.noreply.github.com>
2025-10-04 03:36:16 -0400

    Replaced spritePath with a Reference to a Sprite
    -Items use a texture instead of a path.
    -ItemDatabase loads placeholder potion and sword.
    -Added placeholder potion and sword.

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,40 +1,40 @@
 class_name Item
-	
+    
 var name
 var rarity = Rarity.Normal
 var ID
 
 enum Rarity
 {
-	Normal,
-	Rare,
-	Legendary
+    Normal,
+    Rare,
+    Legendary
 }
 
 enum ItemType
 {
-	Consumable,
-	Equipment
+    Consumable,
+    Equipment
 }
 
 static func Create(baseName, _name=baseName) -> Item:
-	var id = ItemDatabase.FindID(baseName)
-	if id < 0:
-		push_error("Name not found: " + baseName) 
-		return null
-	
-	return Item.new(id, _name)
+    var id = ItemDatabase.FindID(baseName)
+    if id < 0:
+        push_error("Name not found: " + baseName) 
+        return null
+    
+    return Item.new(id, _name)
 
 func _init(id, _name) -> void:
-	ID = id
-	name = _name
-	return
+    ID = id
+    name = _name
+    return
 
 func GetBaseName() -> String:
-	return ItemDatabase.GetName(ID)
+    return ItemDatabase.GetName(ID)
 
 func GetType() -> ItemType:
-	return ItemDatabase.GetType(ID)
+    return ItemDatabase.GetType(ID)
 
-func GetSpritePath() -> String:
-	return ItemDatabase.GetSpritePath(ID)
+func GetSprite() -> Texture:
+    return ItemDatabase.GetSprite(ID)
--- scripts/item_database.gd
+++ scripts/item_database.gd
@@ -1,56 +1,60 @@
 class_name ItemDatabase
 
+#Sprites
+static var potionSprite = preload("res://assets/sprites/potion_placeholder.png")
+static var swordSprite = preload("res://assets/sprites/sword_placeholder.png")
+
 static var _allItems: Array[ItemData] = []
 
 static func _static_init() -> void:
-	RegisterItem("Potion", Item.ItemType.Consumable, "")
-	RegisterItem("Sword", Item.ItemType.Equipment, "")
-	RegisterItem("Bow", Item.ItemType.Equipment, "")
+    RegisterItem("Potion", Item.ItemType.Consumable, potionSprite)
+    RegisterItem("Sword", Item.ItemType.Equipment, swordSprite)
+    RegisterItem("Bow", Item.ItemType.Equipment, "")
 
 static func GetName(id) -> String:
-	return _allItems[id].name
+   return _allItems[id].name
 
 static func FindID(name) -> int:
-	var i = 0
-	for itemData in _allItems:
-		if name == itemData.name:
-			return i
-		i += 1
-		
-	#Not found
-	return -1
+    var i = 0
+    for itemData in _allItems:
+        if name == itemData.name:
+            return i
+        i += 1
+        
+    #Not found
+    return -1
 
 static func GetType(id) -> Item.ItemType:
-	return _allItems[id].type
+    return _allItems[id].type
 
 static func FindType(name):
-	for itemData in _allItems:
-		if name == itemData:
-			return itemData.type
-			
-	#Not found
-	return null
+    for itemData in _allItems:
+        if name == itemData:
+            return itemData.type
+            
+    #Not found
+    return null
 
-static func GetSpritePath(id) -> String:
-	return _allItems[id].spritePath
+static func GetSprite(id) -> Texture:
+    return _allItems[id].sprite
 
-static func FindSpritePath(name) -> String:
-	for itemData in _allItems:
-		if name == itemData:
-			return itemData.spritePath
-		
-	#Not found
-	return ""
+static func FindSprite(name) -> Texture:
+    for itemData in _allItems:
+        if name == itemData:
+            return itemData.sprite
+        
+    #Not found
+    return null
 
 class ItemData:
-	var name
-	var spritePath
-	var type
-	
-static func RegisterItem(name, type, spritePath):
-	var newItem:ItemData = ItemData.new()
-	newItem.name = name
-	newItem.type = type
-	newItem.spritePath = spritePath
-	
-	_allItems.append(newItem)
+    var name
+    var sprite
+    var type
+    
+static func RegisterItem(name, type, sprite):
+    var newItem:ItemData = ItemData.new()
+    newItem.name = name
+    newItem.type = type
+    newItem.sprite = sprite
+    
+    _allItems.append(newItem)
