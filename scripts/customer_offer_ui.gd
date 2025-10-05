ceb4f053277d3e5829b1535f6806a0507926d962
seel <mitch.rogers88@gmail.com>
2025-10-05 12:55:39 -0400

    Created Button Theme & Modified Customer Offer UI
    + Created a Button Theme with styling for solid color backgrounds
    + Modified the Customer Offer window with fonts, outlines, and button themes.
    - Commented out _color_rect as it is no longer needed, but kept it just in case.

--- scripts/customer_offer_ui.gd
+++ scripts/customer_offer_ui.gd
@@ -3,44 +3,45 @@ extends Node2D
 
 signal offer_result(OfferResult, final_price: int)
 
-@onready var _color_rect: ColorRect=  $ColorRect
-@onready var _customer_name_label: Label =  $ColorRect/MarginContainer/VBoxContainer/CustomerNameLabel
-@onready var _flavor_label: Label = $ColorRect/MarginContainer/VBoxContainer/HBoxContainer/FlavorLabel
-@onready var _offer_label: Label = $ColorRect/MarginContainer/VBoxContainer/OfferLabel
-@onready var _item_icon: TextureRect = $ColorRect/MarginContainer/VBoxContainer/HBoxContainer3/ItemIcon
-@onready var _item_name_label: Label = $ColorRect/MarginContainer/VBoxContainer/HBoxContainer3/ItemName
-@onready var _item_value_label: Label = $ColorRect/MarginContainer/VBoxContainer/HBoxContainer3/ValueLabel
+#@onready var _color_rect: ColorRect=  $ColorRect
+@onready var _nine_patch_rect: NinePatchRect = $NinePatchRect
+@onready var _customer_name_label: Label =  $NinePatchRect/MarginContainer/VBoxContainer/CustomerNameLabel
+@onready var _flavor_label: Label = $NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer/FlavorLabel
+@onready var _offer_label: Label = $NinePatchRect/MarginContainer/VBoxContainer/OfferLabel
+@onready var _item_icon: TextureRect = $NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer3/ItemIcon
+@onready var _item_name_label: Label = $NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer3/ItemName
+@onready var _item_value_label: Label = $NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer3/ValueLabel
 
 var _offer: CustomerOffer
 
 func set_customer_offer(offer: CustomerOffer):
-    self._offer = offer
-    self._customer_name_label.text = offer.customer.customer_name
-    self._flavor_label.text = offer.flavor_text
-    self._offer_label.text = "Offer: %dgp" % offer.gold_offered
-    self._item_icon.texture = offer.item_wanted.GetSprite()
-    self._item_name_label.text = offer.item_wanted.name
-    self._item_value_label.text = "(value: %dgp)" % offer.item_wanted.GetValue()
+	self._offer = offer
+	self._customer_name_label.text = offer.customer.customer_name
+	self._flavor_label.text = offer.flavor_text
+	self._offer_label.text = "Offer: %dgp" % offer.gold_offered
+	self._item_icon.texture = offer.item_wanted.GetSprite()
+	self._item_name_label.text = offer.item_wanted.name
+	self._item_value_label.text = "(value: %dgp)" % offer.item_wanted.GetValue()
 
 
 func size() -> Vector2:
-    return  self._color_rect.size
+	return  self._nine_patch_rect.size
 
 
 func _on_accept_button_pressed():
-    self.offer_result.emit(OfferResult.accepted, self._offer.gold_offered)
+	self.offer_result.emit(OfferResult.accepted, self._offer.gold_offered)
 
 
 func _on_decline_button_pressed():
-    self.offer_result.emit(OfferResult.declined, self._offer.gold_offered)
+	self.offer_result.emit(OfferResult.declined, self._offer.gold_offered)
 
 
 func _on_haggle_button_pressed():
-    # TODO
-    pass
+	# TODO
+	pass
 
 
 enum OfferResult {
-    accepted,
-    declined,
+	accepted,
+	declined,
 }
