9c1a634e5f65e924d21f3915f8a32d3406d87ab0
Missingreference <Missingreference@users.noreply.github.com>
2025-10-05 23:55:50 -0400

    Implement Character Animator and Generation

--- scripts/character_animator.gd
+++ scripts/character_animator.gd
@@ -1,10 +1,267 @@
+class_name CharacterAnimator
 extends Node2D
 
+enum Hair
+{
+    None,
+    Short,
+    Long,
+    Shaved,
+    Mohawk,
+    Ponytail
+}
+
+enum FacialHair
+{
+    None,
+    ShortBeard,
+    LongBeard,
+    Mustache
+}
+
+enum EyeExpression
+{
+    Neutral,
+    Bored,
+    Sad,
+    Angry,
+    Shocked
+}
+
+enum MouthExpression
+{
+    Neutral,
+    Happy,
+    Sad
+}
+
+enum Shirt
+{
+    Short,
+    Long
+}
+
+#Animations
+ #Body
+var _animation_head = preload("res://animations/character_head.tres")
+var _animation_arms = preload("res://animations/character_arms.tres")
+var _animation_shirt_short = preload("res://animations/character_short_shirt.tres")
+var _animation_shirt_long = preload("res://animations/character_long_shirt.tres")
+var _animation_pants = preload("res://animations/character_pants.tres")
+var _animation_shoes = preload("res://animations/character_shoes.tres")
+var _animation_armor_pieces = preload("res://animations/character_armor_pieces.tres")
+ #Hair
+var _animation_hair_short = preload("res://animations/character_hair_short.tres")
+var _animation_hair_long = preload("res://animations/character_hair_long.tres")
+var _animation_hair_shaved = preload("res://animations/character_hair_shaved.tres")
+var _animation_hair_mohawk = preload("res://animations/character_hair_mohawk.tres")
+var _animation_hair_ponytail = preload("res://animations/character_hair_ponytail.tres")
+ #Facial Hair
+var _animation_short_beard = preload("res://animations/character_short_beard.tres")
+var _animation_long_beard = preload("res://animations/character_long_beard.tres")
+var _animation_mustache = preload("res://animations/character_mustache.tres")
+#Eyes
+var _animation_eyes_neutral = preload("res://animations/character_eyes_normal.tres")
+var _animation_eyes_sad = preload("res://animations/character_eyes_sad.tres")
+var _animation_eyes_bored = preload("res://animations/character_eyes_bored.tres")
+var _animation_eyes_angry = preload("res://animations/character_eyes_angry.tres")
+var _animation_eyes_shocked = preload("res://animations/character_eyes_shocked.tres")
+#Iris
+var _animation_iris_neutral = preload("res://animations/character_iris_normal.tres")
+var _animation_iris_bored = preload("res://animations/character_iris_bored.tres")
+#Mouth
+var _animation_expression_neutral = preload("res://animations/character_expression_normal.tres")
+var _animation_expression_sad = preload("res://animations/character_expression_sad.tres")
+var _animation_expression_happy = preload("res://animations/character_expression_smiling.tres")
+
+#Animators
+@onready var _head_animator: AnimatedSprite2D = $HeadAnimator
+@onready var _eyes_animator: AnimatedSprite2D = $EyesAnimator
+@onready var _iris_animator: AnimatedSprite2D = $IrisAnimator
+@onready var _mouth_animator: AnimatedSprite2D = $MouthAnimator
+@onready var _hair_animator: AnimatedSprite2D = $HairAnimator
+@onready var _pants_animator: AnimatedSprite2D = $PantsAnimator
+@onready var _shirt_animator: AnimatedSprite2D = $ShirtAnimator
+@onready var _arms_animator: AnimatedSprite2D = $ArmsAnimator
+@onready var _armor_animator: AnimatedSprite2D = $ArmorPiecesAnimator
+@onready var _facial_hair_animator: AnimatedSprite2D = $FacialHairAnimator
+@onready var _shoes_animator: AnimatedSprite2D = $ShoesAnimator
+
+var _all_animators: Array[AnimatedSprite2D]
+
 func _ready() -> void:
-    for i in self.get_child_count():
-        get_child(i).play()
-    pass
+    _all_animators = [
+        _head_animator, 
+        _eyes_animator, 
+        _iris_animator,
+        _mouth_animator,
+        _hair_animator,
+        _pants_animator,
+        _shirt_animator,
+        _arms_animator,
+        _armor_animator,
+        _facial_hair_animator,
+        _shoes_animator
+        ]
+    
+    #Defaults
+    _head_animator.sprite_frames = _animation_head
+    _eyes_animator.sprite_frames = _animation_eyes_neutral
+    _iris_animator.sprite_frames = _animation_iris_neutral
+    _mouth_animator.sprite_frames = _animation_expression_neutral
+    _hair_animator.sprite_frames = _animation_hair_short
+    _pants_animator.sprite_frames = _animation_pants
+    _shirt_animator.sprite_frames = _animation_shirt_short
+    _arms_animator.sprite_frames = _animation_arms
+    _armor_animator.sprite_frames = _animation_armor_pieces
+    _facial_hair_animator.sprite_frames = _animation_short_beard
+    _shoes_animator.sprite_frames = _animation_shoes
+    
+    CharacterPresets.set_random_character(self)
+    self.play_walk_animation()
+    
+func reset():
+    for animator in _all_animators:
+        animator.stop()
+        animator.frame = 0
+    
+    _set_animation("idle1")
+    
+func _set_animation(animation: String):
+    for animator in _all_animators:
+        animator.animation = animation
 
+func play_idle1_animation():
+    reset()
+    _set_animation("idle1")
+    for animator in _all_animators:
+        animator.play()
+                
+func play_idle2_animation():
+    reset()
+    _set_animation("idle2")
+    for animator in _all_animators:
+        animator.play()
+        
+func play_idle3_animation():
+    reset()
+    _set_animation("idle3")
+    for animator in _all_animators:
+        animator.play()
+        
+func play_idle4_animation():
+    reset()
+    _set_animation("idle4")
+    for animator in _all_animators:
+        animator.play()
+        
+func play_walk_animation():
+    reset()
+    _set_animation("walk")
+    for animator in _all_animators:
+        animator.play()
+        
+func stop_animating():
+    for animator in _all_animators:
+        animator.pause()
 
-func _process(delta: float) -> void:
-    pass
+func set_hair(hair: Hair):
+    match hair:
+        Hair.None:
+            _hair_animator.visible = false
+        Hair.Short:
+            _hair_animator.visible = true
+            _hair_animator.sprite_frames = _animation_hair_short
+        Hair.Long:
+            _hair_animator.visible = true
+            _hair_animator.sprite_frames = _animation_hair_long
+        Hair.Shaved:
+            _hair_animator.visible = true
+            _hair_animator.sprite_frames = _animation_hair_shaved
+        Hair.Mohawk:
+            _hair_animator.visible = true
+            _hair_animator.sprite_frames = _animation_hair_mohawk
+        Hair.Ponytail:
+            _hair_animator.visible = true
+            _hair_animator.sprite_frames = _animation_hair_ponytail
+            
+func set_facial_hair(facial_hair: FacialHair):
+    match facial_hair:
+        FacialHair.None:
+            _facial_hair_animator.visible = false
+        FacialHair.ShortBeard:
+            _facial_hair_animator.visible = true
+            _facial_hair_animator.sprite_frames = _animation_short_beard
+        FacialHair.LongBeard:
+            _facial_hair_animator.visible = true
+            _facial_hair_animator.sprite_frames = _animation_long_beard
+        FacialHair.Mustache:
+            _facial_hair_animator.visible = true
+            _facial_hair_animator.sprite_frames = _animation_mustache
+    
+func set_eye_expression(eye_expression: EyeExpression):
+    match eye_expression:
+        EyeExpression.Neutral:
+            _eyes_animator.sprite_frames = _animation_eyes_neutral
+            _iris_animator.sprite_frames = _animation_iris_neutral
+        EyeExpression.Bored:
+            _eyes_animator.sprite_frames = _animation_eyes_bored
+            _iris_animator.sprite_frames = _animation_iris_bored
+        EyeExpression.Sad:
+            _eyes_animator.sprite_frames = _animation_eyes_sad
+            _iris_animator.sprite_frames = _animation_iris_neutral
+        EyeExpression.Angry:
+            _eyes_animator.sprite_frames = _animation_eyes_angry
+            _iris_animator.sprite_frames = _animation_iris_neutral
+        EyeExpression.Shocked:
+            _eyes_animator.sprite_frames = _animation_eyes_shocked
+            _iris_animator.sprite_frames = _animation_iris_neutral
+    
+func set_mouth_expression(mouth_expression: MouthExpression):
+    _mouth_animator.visible = true
+    match mouth_expression:
+        MouthExpression.Neutral:
+            _mouth_animator.sprite_frames = _animation_expression_neutral
+        MouthExpression.Happy:
+            _mouth_animator.sprite_frames = _animation_expression_happy
+        MouthExpression.Sad:
+            _mouth_animator.sprite_frames = _animation_expression_sad
+    
+func set_shirt(shirt: Shirt):
+    _shirt_animator.visible = true
+    match shirt:
+        Shirt.Short:
+            self.move_child(_shirt_animator, 6)
+            _shirt_animator.sprite_frames = _animation_shirt_short
+        Shirt.Long:
+            self.move_child(_shirt_animator, 7)
+            _shirt_animator.sprite_frames = _animation_shirt_long
+
+func enable_armor():
+    _armor_animator.visible = true
+    
+func disable_armor():
+    _armor_animator.visible = false
+    
+#Color Functions
+func set_skin_color(color: Color):
+    _head_animator.self_modulate = color
+    _mouth_animator.self_modulate = color
+    _arms_animator.self_modulate = color
+    
+func set_hair_color(color: Color):
+    _hair_animator.self_modulate = color
+    _facial_hair_animator.self_modulate = color
+    
+func set_eye_color(color: Color):
+    _iris_animator.self_modulate = color
+    
+func set_shirt_color(color: Color):
+    _shirt_animator.self_modulate = color
+
+func set_pants_color(color: Color):
+    _pants_animator.self_modulate = color
+    
+func set_shoes_color(color: Color):
+    _shoes_animator.self_modulate = color
+    
---
+++ scripts/character_presets.gd
@@ -0,0 +1,180 @@
+class_name CharacterPresets
+
+static var skin_colors = [
+    # Light Tones
+    Color("fff0e2"), # Very Fair (Cool)
+    Color("ffd9b3"), # Fair (Warm)
+    Color("f9d6b9"), # Fair (Neutral)
+    Color("ffca93"), # Light (Warm)
+    Color("f2b887"), # Light (Peachy)
+    # Medium Tones
+    Color("e3a16e"), # Tan (Warm)
+    Color("d8996e"), # Olive
+    Color("c7855b"), # Dark Tan (Warm)
+    Color("b37b56"), # Mid-Brown (Neutral)
+    # Dark Tones
+    Color("a0694e"), # Mid-Brown (Warm)
+    Color("8c5a45"), # Dark (Neutral)
+    Color("613d2f"), # Dark (Cool)
+    Color("4a2d25")  # Very Dark
+]
+
+# A wide mix of natural and fantasy hair colors.
+static var hair_colors = [
+    # Blondes & Whites
+    Color("d1d1d1"), # Light Grey
+    Color("b9c2c7"), # Silver
+    Color("848d94"), # Steel Grey
+    # Blondes
+    Color("ffe899"), # Platinum Blonde
+    Color("f4d166"), # Golden Blonde
+    Color("d1b48c"), # Ash Blonde
+    Color("d9a066"), # Dirty Blonde
+    # Browns
+    Color("b0744a"), # Light Brown
+    Color("865a3c"), # Medium Brown
+    Color("6e5a4b"), # Ash Brown
+    Color("59382f"), # Dark Brown
+    # Reds
+    Color("d1804c"), # Strawberry Blonde
+    Color("d1624c"), # Ginger
+    Color("ae4c34"), # Auburn
+    # Blacks
+    Color("322522"), # Off-Black
+    Color("1e1e1e"), # Black
+    # Uncommon / Dyed (Muted)
+    Color("6b3b3b"), # Deep Maroon
+    Color("4a546b"), # Slate Blue
+    Color("4a6356")  # Dark Teal
+]
+
+# Common, uncommon, and fantasy eye colors.
+static var eye_colors = [
+    Color("a0afb5"), # Steel Blue
+    Color("5482b4"), # Blue
+    Color("429363"), # Green
+    Color("9d9f59"), # Hazel
+    Color("b0744a"), # Amber
+    Color("744f3a"), # Brown
+    Color("4a2d25"), # Dark Brown
+    Color("322522"), # Almost Black
+    Color("848d94"), # Grey
+]
+
+# A large wardrobe of colors for shirts, jackets, and tops.
+static var shirt_colors = [
+    # --- Whites, Beiges & Greys (Homespun, Undyed, Worn) ---
+    Color("fefefb"), # Bleached Linen
+    Color("e0d6c0"), # Cream / Undyed Wool
+    Color("c8c2b8"), # Light Stone Grey
+    Color("a8a29a"), # Mid Grey / Worn Cloth
+    Color("848d94"), # Slate Grey
+    Color("6b6b6b"), # Charcoal
+    Color("444444"), # Dark Slate
+    # --- Browns (Earth, Leather, Faded) ---
+    Color("bda871"), # Tan / Light Hide
+    Color("a08a6f"), # Faded Brown
+    Color("9d7f63"), # Undyed Leather
+    Color("8c5a45"), # Russet Brown
+    Color("744f3a"), # Earth Brown
+    # --- Muted Natural Dyes ---
+    #Color("b55a4c"), # Terracotta / Madder Red
+    #Color("a24545"), # Faded Crimson / Maroon
+    #Color("d1804c"), # Ochre / Weld Yellow
+    Color("9d9f59"), # Olive / Lichen Green
+    Color("5a6e5a"), # Moss Green
+    Color("3e7d44"), # Forest Green
+    Color("63778a"), # Washed Blue / Woad
+    #Color("4a689d"), # Indigo / Denim
+    Color("5d4a6b"),  # Heather / Muted Purple
+    # Neutrals
+    Color("a0694e"), # Brown
+    Color("848d94"), # Light Grey
+    Color("555555"), # Charcoal
+    Color("222222")  # Black
+]
+
+# A versatile set of colors for pants, shorts, and skirts.
+static var pants_colors = [
+    Color("e0d6c0"), # Cream / Light Khaki
+    Color("bda871"), # Tan / Khaki
+    Color("9d9f59"), # Olive Drab
+    Color("8c5a45"), # Medium Brown
+    Color("5a4332"), # Dark Brown
+    Color("4a546b"), # Dark Slate
+    Color("6b6b6b"), # Charcoal Grey
+    Color("444444"), # Dark Grey
+    Color("333333"), # Off-Black
+    Color("1e1e1e"),  # Black
+    # --- Greys & Blacks ---
+    Color("a8a29a"), # Light Grey Wool
+    Color("848d94"), # Slate Grey
+    Color("6b6b6b"), # Charcoal
+    Color("444444"), # Dark Slate
+    Color("333333"), # Off-Black
+    Color("1e1e1e"), # Black
+    # --- Browns & Tans ---
+    Color("e0d6c0"), # Light Khaki / Canvas
+    Color("bda871"), # Tan
+    Color("a08a6f"), # Faded Brown
+    Color("8c5a45"), # Sturdy Brown
+    Color("744f3a"), # Earth Brown
+    Color("5a4332"), # Dark Earth
+    Color("4a2d25"), # Very Dark Brown
+    # --- Other Muted Tones ---
+    Color("9d9f59"), # Olive Drab
+    Color("4a689d"), # Dark Denim / Indigo
+    Color("3e7d44")  # Forest Green
+]
+
+# A good range of colors for shoes, boots, and footwear.
+static var shoe_colors = [
+    Color("9d7f63"), # Light Leather
+    Color("b0744a"), # Tan Suede
+    Color("865a3c"), # Medium Brown
+    Color("6b4a3a"), # Brown Leather
+    Color("5a382f"), # Dark Leather
+    Color("4a2d25"), # Dark Leather
+    Color("555555"), # Grey
+    Color("222222"), # Black
+    Color("333333"), # Worn Black
+    Color("5a4332"), # Oiled Leather
+]
+
+static func set_random_character(character_animator: CharacterAnimator):
+    set_random_skin_color(character_animator)
+    set_random_hair_color(character_animator)
+    set_random_eye_color(character_animator)
+    set_random_shirt_color(character_animator)
+    set_random_pants_color(character_animator)
+    set_random_shoe_color(character_animator)
+    
+    var enable_armor = Globals.rng.randi_range(0, 2) == 0
+    if enable_armor:
+        character_animator.enable_armor()
+    else:
+        character_animator.disable_armor()
+    
+    character_animator.set_eye_expression(RandomUtils.pick_random_enum(CharacterAnimator.EyeExpression))
+    character_animator.set_facial_hair(RandomUtils.pick_random_enum(CharacterAnimator.FacialHair))
+    character_animator.set_hair(RandomUtils.pick_random_enum(CharacterAnimator.Hair))
+    character_animator.set_mouth_expression(RandomUtils.pick_random_enum(CharacterAnimator.MouthExpression))
+    character_animator.set_shirt(RandomUtils.pick_random_enum(CharacterAnimator.Shirt))
+
+static func set_random_skin_color(character_animator: CharacterAnimator):
+    character_animator.set_skin_color(RandomUtils.pick_random(skin_colors))
+
+static func set_random_hair_color(character_animator: CharacterAnimator):
+    character_animator.set_hair_color(RandomUtils.pick_random(hair_colors))
+
+static func set_random_eye_color(character_animator: CharacterAnimator):
+    character_animator.set_eye_color(RandomUtils.pick_random(eye_colors))
+    
+static func set_random_shirt_color(character_animator: CharacterAnimator):
+    character_animator.set_shirt_color(RandomUtils.pick_random(shirt_colors))
+    
+static func set_random_pants_color(character_animator: CharacterAnimator):
+    character_animator.set_pants_color(RandomUtils.pick_random(pants_colors))
+
+static func set_random_shoe_color(character_animator: CharacterAnimator):
+    character_animator.set_shoes_color(RandomUtils.pick_random(shoe_colors))
