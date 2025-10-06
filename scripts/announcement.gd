545fd8044b290d11752fd1ca4e9256fbfef2d907
Missingreference <Missingreference@users.noreply.github.com>
2025-10-06 06:42:08 -0400

    Implement Announcement Script

---
+++ scripts/announcement.gd
@@ -0,0 +1,87 @@
+extends Control
+
+@onready var color_rect: ColorRect = $ColorRect
+@onready var label: Label = $"ColorRect/CenterContainer/Center Point/Label"
+
+var background_animate_time = 0.75
+var text_animate_time = 0.75
+var background_size = 250.0
+
+func _ready() -> void:    
+    pass
+
+func announce(message1: String, message2: String):
+    #init label
+    label.modulate.a = 0
+    
+    var vertical_center = get_viewport_rect().size.y / 2
+    color_rect.size.y = 0.0
+    
+    color_rect.global_position.x = 0
+    color_rect.global_position.y = vertical_center
+    
+    var tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_OUT)
+    tween.tween_property(color_rect, "size:y", background_size, background_animate_time)
+    tween.tween_property(color_rect, "position:y", vertical_center - background_size / 2.0, background_animate_time)
+    
+    await tween.finished
+    
+    label.text = message1
+    label.position.y = -(color_rect.size.y / 2.0)
+    var label_target_pos_y = 0 - (label.size.y/2.0)
+    
+    tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_OUT)
+    tween.tween_property(label, "modulate:a", 1.0, text_animate_time - 0.1)
+    tween.tween_property(label, "position:y", label_target_pos_y, text_animate_time)
+    
+    await tween.finished
+    
+    label_target_pos_y = (color_rect.size.y / 2.0) - label.size.y
+    
+    tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_IN)
+    tween.tween_property(label, "modulate:a", 0.0, text_animate_time - 0.1)
+    tween.tween_property(label, "position:y", label_target_pos_y, text_animate_time)
+    
+    await tween.finished
+    
+    label.text = message2
+    label.position.y = -(color_rect.size.y / 2.0)
+    label_target_pos_y = 0 - (label.size.y/2.0)
+    
+    tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_OUT)
+    tween.tween_property(label, "modulate:a", 1.0, text_animate_time - 0.1)
+    tween.tween_property(label, "position:y", label_target_pos_y, text_animate_time)
+    
+    await tween.finished
+    
+    label_target_pos_y = (color_rect.size.y / 2.0) - label.size.y
+    
+    tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_IN)
+    tween.tween_property(label, "modulate:a", 0.0, text_animate_time - 0.1)
+    tween.tween_property(label, "position:y", label_target_pos_y, text_animate_time)
+    
+    await tween.finished
+    
+    tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_OUT)
+    tween.tween_property(color_rect, "size:y", 0, background_animate_time)
+    tween.tween_property(color_rect, "position:y", vertical_center, background_animate_time)
+    
+    await tween.finished
